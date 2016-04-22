054484060c4cd4ef36b1cf1a3d7a1aa6af160022
federuiz <ruizfederico95@gmail.com>
2016-04-22 15:56:25 -0300

    player w/ script

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float x = Input.GetAxis ("Vertical");
+		gameObject.transform.Translate (UtilityClass.walkDirection * speed * x * Time.deltaTime);
+
+	}
+}
+
---
+++ Assets/Scripts/Util/UtilityClass.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class UtilityClass {
+
+	public static Vector3 walkDirection;
+
+}
