6e1ad5a6c77b66197e3100f5c7c4c13b50c3a2c4
Nico Burroni <nmburroni@gmail.com>
2016-05-20 14:55:38 -0300

    More advancements on fireball

---
+++ Assets/Scripts/PowerUp/Fireball.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fireball : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision c)
+    {
+        if (c.gameObject.tag == "Goomba")
+        {
+            c.gameObject.SendMessage("Kill");
+        }
+    }
+}
