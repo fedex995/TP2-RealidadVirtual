efb32b5353fa8025852204c39e272262dd9d66e8
Nico Burroni <nmburroni@gmail.com>
2016-05-21 00:16:26 -0300

    Fixed some coins.

--- Assets/Scripts/Game/Coin.cs
+++ Assets/Scripts/Game/Coin.cs
@@ -4,10 +4,13 @@ using System.Collections;
 public class Coin : MonoBehaviour {
 
 	void OnTriggerEnter(Collider c){
-		Debug.Log ("You got a coin!");
-		c.gameObject.SendMessage ("CollectedCoin");
-		SoundManager.instance.PlaySound (SoundManager.Sounds.Coin);
-		Destroy(this.gameObject);
+	    if (c.gameObject.tag == "Player")
+	    {
+	        Debug.Log("You got a coin!");
+	        c.gameObject.SendMessage("CollectedCoin");
+	        SoundManager.instance.PlaySound(SoundManager.Sounds.Coin);
+	        Destroy(this.gameObject);
+	    }
 	}
 	void Update(){
 		this.transform.Rotate (new Vector3 (0,2,0));
